3d97564f7e4503a730145f25289385fa201c4508
Shaeen <shaeenkevinsingh@gmail.com>
2020-04-05 20:20:45 +0200

    Making form dynamic for user login and registering new user

--- app/controllers/AuthCtrl.js
+++ app/controllers/AuthCtrl.js
@@ -1,15 +1,34 @@
 ;(function () {
   angular.module('TodoApp').controller('AuthCtrl', [
     '$scope',
-    function ($scope) {
+    '$location',
+    function ($scope, $location) {
       constructor()
 
-      $scope.submitForm = function () {
-        console.log(`${$scope.email} ${$scope.password}`)
+      $scope.submit = function () {
+        $scope.dataLoading = true
+        if ($scope.showLoginForm) {
+          console.log('Handle User login')
+          $scope.dataLoading = false
+        } else {
+          console.log('Handle User registration')
+          $scope.dataLoading = false
+        }
       }
 
       function constructor() {
-        // any presets/checks goes here
+        // check path to see which form to display
+        $scope.showLoginForm = true
+        $scope.formMessage = "Are you new? why don't you create an account "
+        $scope.formLink = '/register'
+        $scope.formBtnName = 'Login'
+        var path = $location.path()
+        if (path === '/register') {
+          $scope.showLoginForm = false
+          $scope.formMessage = 'Already have an account? just login '
+          $scope.formLink = '/login'
+          $scope.formBtnName = 'Register'
+        }
       }
     }
   ])
