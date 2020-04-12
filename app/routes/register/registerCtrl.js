5cdf8f9548da9ae74098677f71b3b078a1f21a2a
Shaeen <shaeenkevinsingh@gmail.com>
2020-04-12 15:34:08 +0200

    using Immediate-Invoked Function Expression

--- app/routes/register/registerCtrl.js
+++ app/routes/register/registerCtrl.js
@@ -1,63 +1,65 @@
-angular.module('app.register', []).component('register', {
-  templateUrl: 'app/routes/register/register.html',
-  controllerAs: '$ctrl',
-  controller: [
-    '$location',
-    '$window',
-    'authenticationService',
-    function ($location, $window, authenticationService) {
-      var $ctrl = this
+;(function () {
+  angular.module('app.register', []).component('register', {
+    templateUrl: 'app/routes/register/register.html',
+    controllerAs: '$ctrl',
+    controller: [
+      '$location',
+      '$window',
+      'authenticationService',
+      function ($location, $window, authenticationService) {
+        var $ctrl = this
 
-      // initializing the authentication form object
-      $ctrl.registerFormDetails = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: ''
-      }
+        // initializing the authentication form object
+        $ctrl.registerFormDetails = {
+          firstName: '',
+          lastName: '',
+          email: '',
+          password: ''
+        }
 
-      /**
-       * isUserAuthenticated(response):
-       *
-       * Takes response from both authenticationService.Login() and authenticationService.Register()
-       * handles both success and error cases
-       *
-       * If success
-       *      call authenticationService.SetCredentials() -> which sets response data to localStorage
-       * else
-       *      sets error message that will be displayed in the view
-       *      and loading animation to false which stops loading bar from being displayed
-       */
-      function isUserAuthenticated(response) {
-        if (response.success) {
-          authenticationService.SetCredentials(response.data)
-          $location.path(['/tasks'])
-          delete $ctrl.error
-          $window.location.reload()
-        } else {
-          $ctrl.error = response.message || response.error
-          $ctrl.dataLoading = false
+        /**
+         * isUserAuthenticated(response):
+         *
+         * Takes response from both authenticationService.Login() and authenticationService.Register()
+         * handles both success and error cases
+         *
+         * If success
+         *      call authenticationService.SetCredentials() -> which sets response data to localStorage
+         * else
+         *      sets error message that will be displayed in the view
+         *      and loading animation to false which stops loading bar from being displayed
+         */
+        function isUserAuthenticated(response) {
+          if (response.success) {
+            authenticationService.SetCredentials(response.data)
+            $location.path(['/tasks'])
+            delete $ctrl.error
+            $window.location.reload()
+          } else {
+            $ctrl.error = response.message || response.error
+            $ctrl.dataLoading = false
+          }
+        }
+
+        /**
+         * $ctrl.handleRegistration():
+         * Call authenticationService.Register()
+         * passing `$ctrl.registerFormDetails`
+         *
+         * Sevice will return an error or success response
+         */
+        $ctrl.handleRegistration = function () {
+          $ctrl.dataLoading = true
+          authenticationService.Register($ctrl.registerFormDetails).then(
+            function (res) {
+              isUserAuthenticated(res)
+            },
+            function (res) {
+              isUserAuthenticated(res)
+            }
+          )
         }
       }
-
-      /**
-       * $ctrl.handleRegistration():
-       * Call authenticationService.Register()
-       * passing `$ctrl.registerFormDetails`
-       *
-       * Sevice will return an error or success response
-       */
-      $ctrl.handleRegistration = function () {
-        $ctrl.dataLoading = true
-        authenticationService.Register($ctrl.registerFormDetails).then(
-          function (res) {
-            isUserAuthenticated(res)
-          },
-          function (res) {
-            isUserAuthenticated(res)
-          }
-        )
-      }
-    }
-  ]
-})
+    ]
+  })
+})()
